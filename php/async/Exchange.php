41e73626ceffd1347deebe6a02127e2a2b96ab50
dav1d8 <baranyai8david@gmail.com>
2022-06-04 17:44:29 +0200

    Merge version 1.84.78

--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -28,11 +28,11 @@ use Exception;
 
 include 'Throttle.php';
 
-$version = '1.84.72';
+$version = '1.84.78';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '1.84.72';
+    const VERSION = '1.84.78';
 
     public static $loop;
     public static $kernel;
