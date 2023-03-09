54ab00084fe70a3dde060da4012a3710b0b17bc0
dav1d8 <baranyai8david@gmail.com>
2023-03-09 14:17:19 +0100

    gunzip and inflate need needs to verify the data's first bytes to know whether it's really gzipped ot deflated

--- js/pro/base/functions.js
+++ js/pro/base/functions.js
@@ -3,6 +3,9 @@
 const { inflateRawSync, gunzipSync } = require ('zlib')
 
 function inflate (data) {
+    if (!isDeflate(data)) {
+        return data;
+    }
     return inflateRawSync (data).toString ()
 }
 
@@ -11,7 +14,18 @@ function inflate64 (data) {
 }
 
 function gunzip (data) {
+    if (!isGzip(data)) {
+        return data;
+    }
     return gunzipSync (data).toString ()
+}
+
+function isGzip (data) {
+    return data[0] === 0x1f && data[1] === 0x8b;
+}
+
+function isDeflate (data) {
+    return data[0] === 0x78 && data[1] === 0x9c;
 }
 
 module.exports = {
